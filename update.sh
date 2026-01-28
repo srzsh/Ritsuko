5b18635ef2e6dfa82ea8b469e113490168665e97
srsh <srsh@magisystem.xyz>
2026-01-28 02:23:13 +0100

    Generalised file copying and decrypting, and made the update script work on hostname passed as parameter

--- update.sh
+++ update.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/bash
 set -ex
-rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 all/etc/ balthasar/etc/ balthasar:/etc
-rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 --delete-delay all/containers/ balthasar/containers/ balthasar:/etc/containers/systemd
-rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 balthasar/config/ balthasar:/etc/magisystem
-ssh balthasar sudo systemctl daemon-reload
-ssh balthasar bash <<-'EOF'
-  export SOPS_AGE_KEY_FILE="$XDG_CONFIG_HOME/sops/age/keys.txt"
-  sudo --preserve-env=SOPS_AGE_KEY_FILE find /etc/magisystem -type f -exec bash -c 'grep '\'sops\'\ \'{}\'' && sops decrypt -i '\'{}\' \;
+
+function setup-host {
+  REMOTE_HOST_NAME="$1"
+  rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 all/etc/ "$REMOTE_HOST_NAME"/etc/ "$REMOTE_HOST_NAME":/etc
+  rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 --delete-delay all/containers/ "$REMOTE_HOST_NAME"/containers/ "$REMOTE_HOST_NAME":/etc/containers/systemd
+  rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 "$REMOTE_HOST_NAME"/config/ "$REMOTE_HOST_NAME":/etc/magisystem
+  ssh "$REMOTE_HOST_NAME" sudo systemctl daemon-reload
+  ssh "$REMOTE_HOST_NAME" bash <<-'EOF'
+    export SOPS_AGE_KEY_FILE="$XDG_CONFIG_HOME/sops/age/keys.txt"
+    sudo --preserve-env=SOPS_AGE_KEY_FILE find /etc/magisystem -type f -exec bash -c 'grep '\'sops\'\ \'{}\'' && sops decrypt -i '\'{}\' \;
 EOF
+}
+
+if [ -z ${1:+set} ]; then
+  setup-host balthasar
+  setup-host ayanami
+else
+  setup-host "$1"
+fi
