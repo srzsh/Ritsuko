6c0c6f6125bf7508524d1f3ddec5c314578e573e
srsh <srsh@magisystem.xyz>
2026-02-16 00:11:11 +0100

    Symlinked the traefik config so that the two hosts can share it for now

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ function setup-host {
   REMOTE_HOST_NAME="$1"
   rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 all/etc/ "$REMOTE_HOST_NAME"/etc/ "$REMOTE_HOST_NAME":/etc
   rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 --delete-delay all/containers/ "$REMOTE_HOST_NAME"/containers/ "$REMOTE_HOST_NAME":/etc/containers/systemd
-  rsync -avuP --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 "$REMOTE_HOST_NAME"/config/ "$REMOTE_HOST_NAME":/etc/magisystem
+  rsync -avuPL --rsync-path 'sudo rsync' --usermap=1000:0 --groupmap=1000:0 "$REMOTE_HOST_NAME"/config/ "$REMOTE_HOST_NAME":/etc/magisystem
   ssh "$REMOTE_HOST_NAME" sudo systemctl daemon-reload
   ssh "$REMOTE_HOST_NAME" bash <<-'EOF'
     export SOPS_AGE_KEY_FILE="$XDG_CONFIG_HOME/sops/age/keys.txt"
